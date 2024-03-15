9a9d07a14fde0b51926fa9aaec304027f6eeb934
Amin <aminakbari733@gmail.com>
2024-03-15 11:22:59 +0330

    Update Item.php

--- app/Livewire/Post/Item.php
+++ app/Livewire/Post/Item.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Post;
 
 use Livewire\Component;
-
+use App\Models\Post;
 class Item extends Component
 {
+    public $post;
+
     public function render()
     {
         return view('livewire.post.item');
