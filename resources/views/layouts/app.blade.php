37422a62092c80bb9afac907d0941095d8dc364f
Amin <aminakbari733@gmail.com>
2024-03-15 08:54:07 +0330

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,40 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <style>
-            [x-cloak]{
-                display: none !important;
-            }
-        </style>
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <div class="drawer lg:drawer-open">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center justify-center">
-              <!-- Page content here -->
-              <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-                {{$slot}}
+    <style>
+        [x-cloak]{
+            display: none!important;
+        }
+    </style>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class="font-sans antialiased">
+
+    <div class="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+
+        <main class="drawer-content  w-full bg-white  items-center justify-center">
+            <!-- Page Content -->
+            <div>
+                {{ $slot }}
             </div>
-            <div class="drawer-side">
-              <label for="my-drawer-2" class="drawer-overlay"></label>
+        </main>
 
-              @include('layouts.sidebar')
+        <aside class="drawer-side ">
+            <label for="my-drawer-2" class="drawer-overlay"></label>
 
-            </div>
-          </div>
+            @include('layouts.sidebar')
 
-    </body>
+        </aside>
+    </div>
+
+</body>
+
 </html>
