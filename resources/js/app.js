dba6bb532380b01d786e5e0a4cdbf0a8bf90c7c9
Amin <aminakbari733@gmail.com>
2024-03-15 09:24:00 +0330

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,27 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+//import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+window.Swiper=Swiper;
+window.Navigation=Navigation;
+window.Pagination=Pagination;
+// init Swiper:
+//const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  //modules: [Navigation, Pagination],
+   // If we need pagination
+  // pagination: {
+   // el: '.swiper-pagination',
+ // },
+   // Navigation arrows
+   //navigation: {
+   // nextEl: '.swiper-button-next',
+   // prevEl: '.swiper-button-prev',
+ // },
+//});
