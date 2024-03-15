844718c6c344072ff0c3ff7e6df5a10620931747
Amin <aminakbari733@gmail.com>
2024-03-15 08:30:45 +0330

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,28 +11,30 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <style>
+            [x-cloak]{
+                display: none !important;
+            }
+        </style>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
+
         <div class="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
             <div class="drawer-content flex flex-col items-center justify-center">
               <!-- Page content here -->
               <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
+                {{$slot}}
             </div>
             <div class="drawer-side">
-              <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-              <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                <!-- Sidebar content here -->
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
-              </ul>
+              <label for="my-drawer-2" class="drawer-overlay"></label>
+
+              @include('layouts.sidebar')
 
             </div>
           </div>
-
 
     </body>
 </html>
