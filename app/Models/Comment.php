d86a741af12e2e083d6a141c4ce1e65162accb2f
Amin <aminakbari733@gmail.com>
2024-03-16 11:37:39 +0330

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Overtrue\LaravelLike\Traits\Likeable;
 class Comment extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Likeable;
 
     protected $guarded=[];
 
