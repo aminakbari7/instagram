246bb23512d078fd10e21772afbe2b6f511ecfda
Amin <aminakbari733@gmail.com>
2024-03-16 10:26:08 +0330

    Update Item.php

--- app/Livewire/Post/View/Item.php
+++ app/Livewire/Post/View/Item.php
@@ -2,16 +2,56 @@
 
 namespace App\Livewire\Post\View;
 
+use App\Models\Comment;
 use App\Models\Post;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class Item extends Component
 {
 
+
     public Post $post;
+
+    public $body;
+    public $parent_id=null;
+
+
+    function addComment()  {
+
+        $this->validate(['body'=>'required']);
+
+        #create comment
+        Comment::create([
+            'body'=>$this->body,
+            'parent_id'=>$this->parent_id,
+            'commentable_id'=>$this->post->id,
+            'commentable_type'=>Post::class,
+            'user_id'=>auth()->id()
+
+        ]);
+
+        $this->reset( ['body']);
+
+
+    }
+
+    function setParent(Comment $comment)  {
+
+        $this->parent_id=$comment->id;
+        $this->body="@".$comment->user->name;//we will use username in the future
+    }
 
     public function render()
     {
-        return view('livewire.post.view.item');
+
+
+        $comments = $this->post->comments()->whereDoesntHave('parent')->get();
+
+
+
+
+        return view('livewire.post.view.item',['comments'=>$comments]);
     }
 }
