6a2e50655a036841f323948764826bb0e6ffdbf1
Amin <aminakbari733@gmail.com>
2024-03-16 08:38:08 +0330

    Update Home.php

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -3,40 +3,81 @@
 namespace App\Livewire;
 
 use App\Models\Post;
-use Livewire\Attributes\On;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
+use Livewire\Attributes\On;
+use Livewire\WithPagination;
 
 class Home extends Component
 {
 
+    use WithPagination;
+
     public $posts;
 
+    public $canLoadMore;
+
+    public $perPageIncrement = 5;
+
+    public $perPage = 10;
+
+
+
+    #!!!import use Livewire\Attributes\On;
     #[On('closeModal')]
-    function reverUrl()
+    public function revertUrl()
     {
-        $this->js("history.replaceState({},'','/')");
+
+        $this->js("history.replaceState({}, '', '/')");
     }
 
 
+    #!!!import use Livewire\Attributes\On;
     #[On('post-created')]
-    function postCreaed($id)
+    function postCreated($id)
     {
 
         $post = Post::find($id);
+        $this->posts =  $this->posts->prepend($post);
+    }
 
-        $this->posts = $this->posts->prepend($post);
+
+    /*
+     * --------------------------
+     * Load more posts
+     *---------------------------*/
+
+    public function loadMore()
+    {
+        if (!$this->canLoadMore) {
+            return null;
+        }
+        #increment page
+        $this->perPage += $this->perPageIncrement;
+
+        #load posts
+        $this->loadPosts();
+    }
+
+
+
+    #function to laod posts
+    function loadPosts()
+    {
+        $this->posts = Post::with('comments.replies')->latest()
+            ->take($this->perPage)->get();
+        $this->canLoadMore = (count($this->posts) >= $this->perPage);
     }
 
 
     function mount()
     {
-
-        $this->posts = Post::with('comments')->latest()->get();
-
+        $this->loadPosts();
     }
 
     public function render()
     {
+
         return view('livewire.home');
     }
 }
