d36c9d795c6629d741558c4313ad574f561d117e
Amin <aminakbari733@gmail.com>
2024-03-16 13:06:35 +0330

    Update item.blade.php

--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -1,10 +1,13 @@
 <div class="max-w-lg mx-auto  ">
+
     {{------------}}
     {{--Header----}}
     {{------------}}
+
+
     <header class="flex items-center gap-3">
 
-        <x-avatar src="https://source.unsplash.com/500x500?face-{{rand(0,10)}}" class="w-9 h-9" />
+        <x-avatar wire:ignore  src="https://source.unsplash.com/500x500?face-{{rand(0,10)}}" class="w-9 h-9" />
 
         <div class="grid grid-cols-7 w-full gap-2">
             <div class="col-span-5">
@@ -35,9 +38,8 @@
 
     <main>
 
-        <div class="my-2">
-            <div
-            x-init="
+        <div wire:ignore class="my-2">
+            <div x-init="
             new Swiper($el, {
                    modules: [Navigation, Pagination],
                    loop: true,
@@ -52,8 +54,7 @@
                    },
 
                    });
-          "
-            class="swiper  h-[500px] border bg-white">
+          " class="swiper  h-[500px] border bg-white">
 
                 <!-- Slides -->
                 <ul x-cloak class="swiper-wrapper   ">
@@ -61,19 +62,18 @@
                     @foreach ($post->media as $file)
                     <li class="swiper-slide">
                         @switch($file->mime)
-                            @case('video')
+                        @case('video')
 
-                                <x-video  source="{{$file->url}}" />
-                                @break
-                            @case('image')
-                            <img class="h-[500px] w-full block object-scale-down "
-                            src="{{$file->url}}" alt="image">
-                                @break
-                            @default
+                        <x-video source="{{$file->url}}" />
+                        @break
+                        @case('image')
+                        <img class="h-[500px] w-full block object-scale-down " src="{{$file->url}}" alt="image">
+                        @break
+                        @default
 
                         @endswitch
 
-                   </li>
+                    </li>
 
 
                     @endforeach
@@ -123,6 +123,7 @@
         {{-- actions --}}
         <div class="flex gap-4 items-center my-2">
 
+            {{-- like --}}
             @if ($post->isLikedBy(auth()->user()))
 
             <button wire:click="togglePostLike()">
@@ -141,13 +142,20 @@
             </button>
             @endif
 
-            <span>
+
+
+
+            @if ($post->allow_commenting)
+            <span
+                onclick="Livewire.dispatch('openModal',{ component: 'post.view.modal', arguments:{'post':{{$post->id}}}})">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
                 </svg>
             </span>
+            @endif
+
 
             <span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -164,23 +172,31 @@
                         d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                 </svg>
             </span>
+
         </div>
 
         {{-- Likes or views --}}
-        <p class="font-bold text-sm">103,564 likes</p>
+        @if ($post->totalLikers>0 && !$post->hide_like_view)
+        <p class="font-bold text-sm">{{$post->totalLikers}} {{$post->totalLikers>1?'likes':'like'}}</p>
+        @endif
 
         {{-- name & comment --}}
         <div class=" flex text-sm gap-2 font-medium ">
             <p> <strong class="font-bold">{{$post->user->name}}</strong>
-            {{$post->description}}
+                {{$post->description}}
             </p>
         </div>
 
+        @if ($post->allow_commenting)
+
         {{-- View Post Modal--}}
-        <button onclick="Livewire.dispatch('openModal',{ component: 'post.view.modal', arguments:{'post':{{$post->id}}}})" class="text-slate-500/90 text-sm font-medium">View all {{$post->comments->count()}} comments</button>
+        <button
+            onclick="Livewire.dispatch('openModal',{ component: 'post.view.modal', arguments:{'post':{{$post->id}}}})"
+            class="text-slate-500/90 text-sm font-medium">View all {{$post->comments->count()}} comments</button>
+
 
         @auth
-        {{-- Show auth comments  --}}
+        {{-- Show auth comments --}}
         <ul class="my-2">
             @foreach ($post->comments()->where('user_id',auth()->id())->get() as $comment)
             <li class="grid grid-cols-12 text-sm items-center">
@@ -189,10 +205,24 @@
                 <span class="col-span-9">{{$comment->body}}</span>
                 <button class="col-span-1  mb-auto flex justify-end pr-px">
                     {{-- heart from heroicons --}}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.9" stroke="currentColor" class="w-3 h-3">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                      </svg>
+                    @if ($comment->isLikedBy(auth()->user()))
+
+                    <span wire:click="toggleCommentLike({{$comment->id}})">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3 h-3 text-rose-500">
+                            <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                          </svg>
+                    </span>
+                    @else
+                    <span wire:click="toggleCommentLike({{$comment->id}})">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.9"
+                            stroke="currentColor" class="w-3 h-3">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        </svg>
+                    </span>
+                    @endif
                 </button>
+
 
 
             </li>
@@ -202,11 +232,10 @@
         @endauth
 
 
-        {{-- Leave comment --}}
-
-        <form
-        wire:key='{{time()}}'
-        @submit.prevent="$wire.addComment()" class="grid grid-cols-12 items-center w-full  " x-data="{ body: @entangle('body') }">
+        {{-- Comment --}}
+        {{-- Check if commendint is allowed --}}
+        <form wire:key='{{time()}}' @submit.prevent="$wire.addComment()" class="grid grid-cols-12 items-center w-full  "
+            x-data="{ body: @entangle('body') }">
             @csrf
             <input placeholder="Leave a comment" type="text"
                 class="border-0 col-span-10 placeholder:text-sm text-sm outline-none w-full focus:outline-none px-0 rounded-lg hover:ring-0 focus:ring-0"
@@ -227,6 +256,8 @@
 
             </span>
         </form>
+        @endif
+
 
     </footer>
 
