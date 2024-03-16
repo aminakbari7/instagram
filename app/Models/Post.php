b62400ceb2fc9c9aae720883417aa55f5a305ff4
Amin <aminakbari733@gmail.com>
2024-03-16 08:34:06 +0330

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -30,4 +30,9 @@ class Post extends Model
 
         return $this->belongsTo(User::class);
     }
+
+    function comments() : MorphMany {
+
+        return  $this->morphMany(Comment::class,'commentable')->with('replies');
+     }
 }
