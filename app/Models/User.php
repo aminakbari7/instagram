43dbd3ef846a61447a9b6a5ac9839727419e195b
Amin <aminakbari733@gmail.com>
2024-03-16 08:34:04 +0330

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,8 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
 
     }
+    function comments() : HasMany {
+
+        return $this->hasMany(Comment::class);
+    }
 }
