ce1f2123ccf37ef05b58f989621bcdf9e090d9c1
Amin <aminakbari733@gmail.com>
2024-03-17 10:49:26 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Components\Sidebar;
 use App\Livewire\Explore;
 use App\Livewire\Home;
 use App\Livewire\Profile\Home as ProfileHome;
