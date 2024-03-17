a04c371697a74e374f24688f4e9bbb79daf32ce2
Amin <aminakbari733@gmail.com>
2024-03-17 10:36:30 +0330

    Update Reels.php

--- app/Livewire/Reels.php
+++ app/Livewire/Reels.php
@@ -3,11 +3,35 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Post;
+use Livewire\Attributes\On;
+
+
 
 class Reels extends Component
 {
+
+    #[On('closeModal')]
+    public function revertUrl()
+    {
+        $this->js("history.replaceState({}, '', '/explore')");
+    }
+    function toggleFavorite(Post $post)  {
+
+        abort_unless(auth()->check(),401);
+        auth()->user()->toggleFavorite($post);
+
+    }
+    function togglePostLike(Post $post) {
+
+        abort_unless(auth()->check(),401);
+        auth()->user()->toggleLike($post);
+
+
+    }
     public function render()
     {
-        return view('livewire.reels');
+        $posts= Post::limit(20)->where('type','reel')->get();
+        return view('livewire.reels',['posts'=>$posts]);
     }
 }
