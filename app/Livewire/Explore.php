3485f8282fdc1098dd3d122086dabebd27b35427
Amin <aminakbari733@gmail.com>
2024-03-17 10:15:19 +0330

    Update Explore.php

--- app/Livewire/Explore.php
+++ app/Livewire/Explore.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Explore extends Component
 {
+
+     #!!!import use Livewire\Attributes\On;
+     #[On('closeModal')]
+     public function revertUrl()
+     {
+
+         $this->js("history.replaceState({}, '', '/explore')");
+     }
+
+
     public function render()
     {
-        return view('livewire.explore');
+
+        $posts= Post::limit(20)->get();
+        return view('livewire.explore',['posts'=>$posts]);
     }
 }
